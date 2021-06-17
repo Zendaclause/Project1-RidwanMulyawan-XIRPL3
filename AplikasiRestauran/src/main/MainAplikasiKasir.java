1239290b616f99fab2268c84d0866e76c89f8ca9
Ridwan <rmulyawan63@gmail.com>
2021-06-17 08:45:50 +0700

    add fitur menambah dan menampilkan daftar menu

--- AplikasiRestauran/src/main/MainAplikasiKasir.java
+++ AplikasiRestauran/src/main/MainAplikasiKasir.java
@@ -5,14 +5,65 @@
  */
 package main;
 
+import classes.DaftarMenu;
+import classes.Kuah;
+import classes.Menu;
+import classes.Minuman;
+import classes.Pesanan;
+import classes.Ramen;
+import classes.Toping;
+import classes.Transaksi;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 /**
  *
  * @author admin
  */
 public class MainAplikasiKasir {
+    public DaftarMenu daftarMenu;
 
-    public static void main(String[] args) {}
+    public static final double PAJAK_PPN = 0.10;
+    public static final double BIAYA_SERVICE = 0.05;
 
-    public void generateDaftarMenu(){}
+    public static void main(String[] args) {
+        
+        Scanner input = new Scanner(System.in);
+
+        
+
+        MainAplikasiKasir app = new MainAplikasiKasir();
+        app.generateDaftarMenu();
+
+        
+    }
+   
+    public void generateDaftarMenu(){
+        daftarMenu = new DaftarMenu();
+
+        daftarMenu.tambahMenu(new Ramen("Ramen Seafood", 25000));
+        daftarMenu.tambahMenu(new Ramen("Ramen Original", 18000));
+        daftarMenu.tambahMenu(new Ramen("Ramen Vegetarian", 22000));
+        daftarMenu.tambahMenu(new Ramen("Ramen Karnivor", 28000));
+
+        daftarMenu.tambahMenu(new Kuah("Kuah Orisinil"));
+        daftarMenu.tambahMenu(new Kuah("Kuah Internasional"));
+        daftarMenu.tambahMenu(new Kuah("Kuah Spicy Lada"));
+        daftarMenu.tambahMenu(new Kuah("Kuah Soto Padang"));
+
+        daftarMenu.tambahMenu(new Toping("Crab Stick Bakar", 6000));
+        daftarMenu.tambahMenu(new Toping("Chicken Katsu", 8000));
+        daftarMenu.tambahMenu(new Toping("Gyoza Goreng", 4000));
+        daftarMenu.tambahMenu(new Toping("Bakso Goreng", 7000));
+        daftarMenu.tambahMenu(new Toping("Enoki Goreng", 5000));
+
+        daftarMenu.tambahMenu(new Minuman("Jus Alpukat SPC", 10000));
+        daftarMenu.tambahMenu(new Minuman("Jus Stroberi", 11000));
+        daftarMenu.tambahMenu(new Minuman("Capucino Coffee", 15000));
+        daftarMenu.tambahMenu(new Minuman("Vietnam Dripp", 14000));
+
+        daftarMenu.tampilDaftarMenu();
+    }
+
     
 }
