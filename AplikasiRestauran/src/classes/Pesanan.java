98d00ada55a86ce5e5ab2a1b54b7d8d64aedbefe
Ridwan <rmulyawan63@gmail.com>
2021-06-17 08:16:52 +0700

    Implementasi struktur kelas aplikasi kasir

---
+++ AplikasiRestauran/src/classes/Pesanan.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author admin
+ */
+public class Pesanan {
+    private Menu menu;
+    private int jumlah;
+    private String keterangan;
+    
+    public Pesanan(Menu menu, int jumlah){
+        this.menu = menu;
+        this.jumlah = jumlah;
+    }
+
+
+    public void setKeterangan(String keterangan){
+        
+    }
+    
+    public String getKeterangan(){
+        return "";
+    }
+}
